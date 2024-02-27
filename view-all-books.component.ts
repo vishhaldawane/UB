a22916f04ca0e70caa70b1755bf26eefb6f3b5ef
vishhaldawane <45403067+vishhaldawane@users.noreply.github.com>
2024-02-27 17:14:13 +0530

    Add files via upload

---
+++ view-all-books.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { BookService } from '../book.service';
+import { Book } from '../Book';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-view-all-books',
+  standalone: true,
+  imports: [CommonModule, FormsModule],
+  templateUrl: './view-all-books.component.html',
+  styleUrl: './view-all-books.component.css'
+})
+export class ViewAllBooksComponent implements OnInit { //UI 
+
+  allBooks! : Book[]; //local empty array 
+  constructor(private bookService: BookService) {} //get the service
+  ngOnInit(): void {
+    this.allBooks = this.bookService.getAllBooks(); //invoke service
+  }
+  deleteIt(id: number) {
+    this.bookService.deleteABook(id);
+  }
+  editIt(book: Book) {
+    console.log('current value before editing ',book);
+    this.bookService.editExistingBook(book);
+  }
+
+}
