02377093aa15131e73282fe0e4adf0d5b03993d5
vishhaldawane <45403067+vishhaldawane@users.noreply.github.com>
2024-02-29 17:20:07 +0530

    Add files via upload

--- UserDetailsRepositoryTests.java
+++ UserDetailsRepositoryTests.java
@@ -1,6 +1,7 @@
 package com.example.demo;
 
 import java.util.Iterator;
+import java.util.Optional;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,6 +25,41 @@ class UserDetailsRepositoryTests {
 		user.setCityAddress("Pune");
 		user.setUserAge(25);
 		userRepo.save(user);
+	}
+	
+	@Test
+	void updateUserDetailsTest() {
+		UserDetails user = new UserDetails();
+		user.setUserIdentificationNumber(8);
+		user.setNameOfUser("MARTIN KING");
+		user.setEmailAddress("matrinking@gmail.com");
+		user.setCityAddress("Nagpur");
+		user.setUserAge(26);
+		userRepo.save(user); //if the record exists by this ID, then update
+	}
+	
+	@Test
+	void deleteUserDetailsTest() {
+		UserDetails user = new UserDetails();
+		user.setUserIdentificationNumber(8);
+		userRepo.delete(user);
+	}
+	
+	@Test
+	void findSingleUserTest() {
+		int userId=11;
+		Optional<UserDetails> user = userRepo.findById(userId);
+		if(user.isPresent()) {
+			UserDetails userDetails = user.get();
+			System.out.println("User ID    : "+userDetails.getUserIdentificationNumber());
+			System.out.println("User Name  : "+userDetails.getNameOfUser());
+			System.out.println("User Email : "+userDetails.getEmailAddress());
+			System.out.println("User City  : "+userDetails.getCityAddress());
+			System.out.println("User Age   : "+userDetails.getUserAge());
+		}
+		else {
+			System.out.println("User NOT found : "+userId);
+		}
 	}
 	
 	@Test
