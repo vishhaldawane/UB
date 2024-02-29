1f29042fdbe9b3d29960fc13e4ae6a9d9e06033d
vishhaldawane <45403067+vishhaldawane@users.noreply.github.com>
2024-02-29 11:06:14 +0530

    Add files via upload

--- AssociationTest.java
+++ AssociationTest.java
@@ -1,6 +1,14 @@
 
 public class AssociationTest {
 	public static void main(String[] args) {
+		WashingMachine washingMachine = new WashingMachine();
+		
+		Water h2o= new Water();
+		Electricity relianceEnergy = new Electricity();
+		WashingPowder surfExcel= new WashingPowder();
+		Cloth jean = new Cloth();
+		
+		Laundry laundryBill = washingMachine.wash(h2o, relianceEnergy , surfExcel, jean);
 		
 	}
 }
@@ -12,26 +20,24 @@ public class AssociationTest {
 			  |      |              |             |
 			  structural			operational/functional
 */
-class WashingTub {
-	
-}
-class CleaningDevice //super class
-{
-	
-}
+class WashingTub { }
+class CleaningDevice {} //super class
 class WashingPowder { }
 class Water { }
 class Electricity { }
 class Cloth { }
+class Laundry { }
 		//sub class
 class WashingMachine extends CleaningDevice //isA - Aggregation
 {
-	WashingTub washTub = new WashingTub(); //hasA - Composition
+	WashingTub washTub = new WashingTub(); //hasA - Composition- mandate for the physical presence of a washing machine
 	
 				//usesA - passing object as parameter for 
 				//the sake of running this method
-	void wash(Water water, Electricity elect, WashingPowder powder, Cloth cloth) {
+	Laundry wash(Water water, Electricity elect, WashingPowder powder, Cloth cloth) {
 		
+		Laundry laundry = new Laundry();
+		return laundry;
 	}
 }
 
