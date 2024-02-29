db5345da665039ff6089733d5e556ba008c70f18
vishhaldawane <45403067+vishhaldawane@users.noreply.github.com>
2024-02-29 12:04:59 +0530

    Add files via upload

--- AssociationTest.java
+++ AssociationTest.java
@@ -1,19 +1,36 @@
+//Restaurant restaurant = new Restaurant();
 
-public class AssociationTest {
-	public static void main(String[] args) {
-		
-		//isA Machine, hasA WashingTub
-		WashingMachine washingMachine = new WashingMachine(); 
-		
+//Thali thali = restaurant.getThali();
+
+class WashingMachineFactory // factory 'design pattern' 
+{
+	WashingMachine getWashingMachine(String brandName) {
+		WashingTub washTub = new WashingTub();
+		WashingMachine washingMachine = new WashingMachine(brandName, washTub);
 		Water h2o= new Water();
 		Electricity relianceEnergy = new Electricity();
 		WashingPowder surfExcel= new WashingPowder();
 		Cloth jean = new Cloth();
-		
-												//using these objects
 		Laundry laundry = washingMachine.wash(h2o, relianceEnergy , surfExcel, jean);
-		//producing Laundry
 		laundry.bill();
+		System.out.println("------------------------");
+		return washingMachine;
+	}
+}
+
+public class AssociationTest {
+	public static void main(String[] args) {
+		
+	
+		//isA Machine, hasA WashingTub
+		WashingMachineFactory factory = new WashingMachineFactory();
+		
+		WashingMachine washingMachine1 = factory.getWashingMachine("Samsung"); 
+		WashingMachine washingMachine2 = factory.getWashingMachine("LG");
+		WashingMachine washingMachine3 = factory.getWashingMachine("IFB");
+		WashingMachine washingMachine4 = factory.getWashingMachine("Whirlpool");
+		WashingMachine washingMachine5 = factory.getWashingMachine("Bosch");
+	
 		
 	}
 }
@@ -46,12 +63,21 @@ class Laundry {
 		//sub class
 class WashingMachine extends CleaningDevice //isA - Aggregation
 {
-	WashingTub washTub = new WashingTub(); //hasA - Composition- mandate for the physical presence of a washing machine
 	
+	WashingTub washTub ; //hasA - Composition- mandate for the physical presence of a washing machine
+	String brandName;
+	
+	
+	public WashingMachine(String brandName,WashingTub wt) {
+		super();
+		this.brandName = brandName;
+		washTub = wt;
+	}
+
 				//usesA - passing object as parameter for 
 				//the sake of running this method
 	Laundry wash(Water water, Electricity elect, WashingPowder powder, Cloth cloth) {
-		System.out.println("Washing machine is using the water............");
+		System.out.println(brandName+" Washing machine is using the water............");
 		System.out.println("Washing machine is using the electricity......");
 		System.out.println("Washing machine is using the washing powder...");
 		washTub.rinse();
