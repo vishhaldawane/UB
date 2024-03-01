56f7f58d6217313efd3b4667589bad63ccd7d156
vishhaldawane <45403067+vishhaldawane@users.noreply.github.com>
2024-03-01 15:57:41 +0530

    TEXT RESPONSE

--- user-details.service.ts
+++ user-details.service.ts
@@ -1,7 +1,9 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { UserDetails } from './user-details';
+import { response } from 'express';
+import { text } from 'stream/consumers';
 
 @Injectable({
   providedIn: 'root'
@@ -15,4 +17,12 @@ export class UserDetailsService {
     return this.myHTTP.get<UserDetails[]>(this.baseURL+"/users");
   }
 
+  HTTPOptionsForText: Object = {
+    headers: new HttpHeaders({'Content-Type': 'application/json'}),
+    responseType: 'text'
+ }
+ 
+  deleteUserDetailsService(num:number) : Observable<any> {
+    return this.myHTTP.delete<any>(this.baseURL+"/deleteUser/"+num, this.HTTPOptionsForText);
+  }
 }
